baf5a4b55ce917fbc0831e58f9236a546fdf000b
Subesh Yadav <41501661+subeshyadav3@users.noreply.github.com>
2025-12-20 19:17:44 +0545

    fix

--- frontend/src/app/api/resources/[program]/[semester]/route.ts
+++ frontend/src/app/api/resources/[program]/[semester]/route.ts
@@ -1,1 +1,9 @@
 // used to call the backend API to get the list of resources for a specific program and semester
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  return NextResponse.json({ message: "Hello from API!" });
+}
+
+// Optionally POST or other methods
+// export async function POST(request: Request) { ... }
