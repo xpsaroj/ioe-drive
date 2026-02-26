b1b8e503357831d4bdb970cfa782b51665808818
Saroj <saroj.p.2728@gmail.com>
2026-02-26 14:56:12 +0545

    feat(ui): update Navbar component with toggle functionality for desktop navigation

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -1,5 +1,7 @@
 "use client"
 import { usePathname } from "next/navigation"
+import { useState } from "react"
+import { PanelLeftOpen, PanelRightOpen } from "lucide-react";
 
 import { SearchBar } from "@/components/layout"
 import Button from "@/components/ui/Button"
@@ -14,42 +16,54 @@ import { NAVIGATION_ITEMS } from "@/constants/navigations"
 export default function Navbar() {
   const pathname = usePathname()
 
+  const [showDesktopNav, setShowDesktopNav] = useState(true)
+
   return (
-    <div className="w-64">
-      {/* Sidebar (Desktop) */}
-      <div className="flex flex-col gap-4 overflow-hidden h-screen">
-        <div className="pt-3 px-3 flex flex-col gap-4">
-          <div className="px-3 flex flex-row gap-2 items-center">
-            <Logo theme="dark" size={2} bg={false} />
-            IOE Drive
+    <div className="hidden md:block w-auto relative">
+      <Button
+        variant="ghost"
+        size="sm"
+        className={`absolute top-2 ${showDesktopNav ? "-right-4" : "-right-10"} hidden p-0 md:flex items-center justify-center backdrop-blur-sm shadow-lg`}
+        onClick={() => setShowDesktopNav(!showDesktopNav)}
+        icon={showDesktopNav ? <PanelRightOpen className="size-5" /> : <PanelLeftOpen className="size-5" />}
+        iconOnly
+      />
+
+      <div className={`${showDesktopNav ? "w-64 block" : "hidden"}`}>
+        <div className="flex flex-col gap-4 overflow-hidden h-screen">
+          <div className="pt-3 px-3 flex flex-col gap-4">
+            <div className="px-3 flex flex-row gap-2 items-center">
+              <Logo theme="dark" size={2} bg={false} />
+              IOE Drive
+            </div>
+
+            <div>
+              <SearchBar />
+            </div>
           </div>
 
-          <div>
-            <SearchBar />
-          </div>
-        </div>
-
-        <div className="h-full px-3 flex flex-col overflow-y-auto">
-          <h3 className="text-sm font-semibold text-muted-foreground uppercase tracking-wider mb-2">
-            Navigation
-          </h3>
-          <div className="space-y-1 flex flex-col" role="navigation">
-            {NAVIGATION_ITEMS.map(({ href, icon: Icon, name }, index) => {
-              const isCurrentRoute = pathname === href || pathname.startsWith(href + "/");
-              return (
-                <Button
-                  key={href + index}
-                  href={href}
-                  variant={isCurrentRoute ? "secondary" : "ghost"}
-                  className="border-none w-full justify-start"
-                >
-                  <div className="flex gap-2 items-center">
-                    <Icon className="size-5" />
-                    {name}
-                  </div>
-                </Button>
-              );
-            })}
+          <div className="h-full px-3 flex flex-col overflow-y-auto">
+            <h3 className="text-sm font-semibold text-muted-foreground uppercase tracking-wider mb-2">
+              Navigation
+            </h3>
+            <div className="space-y-1 flex flex-col" role="navigation">
+              {NAVIGATION_ITEMS.map(({ href, icon: Icon, name }, index) => {
+                const isCurrentRoute = pathname === href || pathname.startsWith(href + "/");
+                return (
+                  <Button
+                    key={href + index}
+                    href={href}
+                    variant={isCurrentRoute ? "secondary" : "ghost"}
+                    className="border-none w-full justify-start"
+                  >
+                    <div className="flex gap-2 items-center">
+                      <Icon className="size-5" />
+                      {name}
+                    </div>
+                  </Button>
+                );
+              })}
+            </div>
           </div>
         </div>
 
