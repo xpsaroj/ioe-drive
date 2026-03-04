aad569a3fc88ffff62dbb41ba066896a5cc639b0
Saroj <saroj.p.2728@gmail.com>
2026-03-05 01:54:46 +0545

    feat: add express-rate-limit middleware for API rate limiting, update multer version

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import { clerkMiddleware } from "@clerk/express"
+import rateLimit from "express-rate-limit";
 
 import { isTest, isDev, env } from "./config/env.js";
 import healthRoutes from "./modules/health/health.routes.js";
@@ -11,6 +12,21 @@ import apiRoutes from "./routes/index.js";
 import { errorHandler } from "./middlewares/error.middleware.js";
 
 const app = express();
+
+if (env.NODE_ENV === "production") {
+  app.set("trust proxy", 1);      // Render runs the server behind a reverse proxy, so we need to trust the first proxy for correct IP detection
+}
+
+const apiLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,     // 15 minutes
+    max: 500,                     // limit each IP to 500 requests per window
+    standardHeaders: true,        // Return rate limit info in headers
+    legacyHeaders: false,         // Disable X-RateLimit-* headers
+    message: {
+        success: false,
+        error: "Too many requests, please try again later."
+    }
+});
 
 // CORS Configuration
 app.use(cors({
@@ -40,7 +56,7 @@ app.use(express.json());
 app.use("/health", healthRoutes);
 
 // API Routes
-app.use("/api", apiRoutes)
+app.use("/api", apiLimiter, apiRoutes)
 
 // 404 Handler
 app.use((_req, res) => {
